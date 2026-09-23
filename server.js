da97f416dd08f71a6c89e2cb9ff4bacb6075c333
Littlex456 <Littlex40@gmail.com>
2026-09-23 02:31:30 +0100

    Fix Vercel configuration

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static("Threadlypublic"));
+const sitePath = path.join(__dirname, "Threadlypublic");
+
+// Serve THREADLY files
+app.use(express.static(sitePath));
+
+// Explicit homepage route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(sitePath, "index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`THREADLY server is running on http://localhost:${PORT}`);
+    console.log(`THREADLY server is running on port ${PORT}`);
 });
+
+module.exports = app;
